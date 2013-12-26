6c0e5bcca4f567c9556d477b4bb580e90e3f4303
blaino <blaine.m.nelson@gmail.com>
2013-12-26 07:40:38 -0500

    Preliminary set of working flask unit tests; remove flash from login

--- app/app_tests.py
+++ app/app_tests.py
@@ -1,52 +1,26 @@
-import app
-import unittest
-from app import db
+from flask.ext.testing import TestCase
+from app import app, db
 
 
-class VinneratorTestCase(unittest.TestCase):
+class MyTest(TestCase):
+
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/cap_test_db"
+    TESTING = True
+    DEBUG = True
+
+    def create_app(self):
+        return app
 
     def setUp(self):
-        self.app = app.app.test_client()  # this is really confusing, but works???
+
         db.create_all()
 
     def tearDown(self):
+
         db.session.remove()
         db.drop_all()
 
-    def test_empty_db(self):
-        self.tearDown()
-        rv = self.app.get('/show_scenarios')
-        assert 'No scenarios here so far' in rv.data
 
-    def login(self, username, password):
-        return self.app.post('/login', data=dict(
-            username=username,
-            password=password
-            ), follow_redirects=True)
 
-    def logout(self):
-        return self.app.get('/logout', follow_redirects=True)
 
-    def test_login_logout(self):
-        rv = self.login('admin', 'password')
-        #assert 'You were logged in' in rv.data
-        assert 'log out' in rv.data
-        rv = self.logout()
-        assert 'log in' in rv.data
-        rv = self.login('adminx', 'default')
-        assert 'Invalid username' in rv.data
-        rv = self.login('admin', 'defaultx')
-        assert 'Invalid password' in rv.data
 
-    def test_messages(self):
-        self.login('admin', 'password')
-        rv = self.app.post('/add', data=dict(
-            title='<Hello>',
-            text='<strong>HTML</strong> allowed here'
-        ), follow_redirects=True)
-        assert 'No scenarios here so far' not in rv.data
-        assert '&lt;Hello&gt;' in rv.data
-        assert '<strong>HTML</strong> allowed here' in rv.data
-
-if __name__ == '__main__':
-    unittest.main()
