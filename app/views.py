5f1e5ab094d660557546e00f8cef5d7ae27f2641
blaino <blaine.m.nelson@gmail.com>
2013-12-29 18:12:58 -0500

    Styling login, register; add dummy contact page

--- app/views.py
+++ app/views.py
@@ -31,6 +31,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/basic')
 def basic():
